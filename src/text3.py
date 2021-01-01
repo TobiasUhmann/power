ff8436dd90e3efe221258c580dd2352f68023a93
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-01 23:47:48 +0100

    Use Classifier()

--- src/text3.py
+++ src/text3.py
@@ -1,9 +1,13 @@
 import os
 
+from classifier.classifier import Classifier
+
 
 def main():
     if not os.path.isdir('data/'):
         os.mkdir('data/')
+
+    classifier = Classifier()
 
 
 if __name__ == '__main__':
