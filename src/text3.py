3d1c96594bb5ede5443da3101c8e360e441f68a1
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-02 03:02:53 +0100

    Train classifier

--- src/text3.py
+++ src/text3.py
@@ -1,13 +1,18 @@
-import os
+from pytorch_lightning import Trainer
 
 from power.classifier import Classifier
+from power.data_module import DataModule
 
 
 def main():
-    if not os.path.isdir('data/'):
-        os.mkdir('data/')
+    data_module = DataModule(data_dir='data/', batch_size=64)
 
-    classifier = Classifier()
+    classifier = Classifier(vocab_size=100000,
+                            embed_dim=32,
+                            num_class=4)
+
+    trainer = Trainer()
+    trainer.fit(classifier, data_module)
 
 
 if __name__ == '__main__':
