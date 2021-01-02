39eed035f4ce89fe4530334071a1628fe852c87e
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-02 03:02:26 +0100

    Transform and generate_batches() in DataModule

--- src/power/data_module.py
+++ src/power/data_module.py
@@ -1,17 +1,28 @@
-from typing import Optional
+from typing import Optional, List, Tuple
 
+import torch
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader, random_split
-from torchtext.data import TabularDataset, Field, Dataset
+from torchtext.data import TabularDataset, Field
+
+
+def generate_batch(batch):
+    label = torch.tensor([entry[0] for entry in batch])
+    text = [entry[1] for entry in batch]
+    offsets = [0] + [len(entry) for entry in text]
+
+    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+    text = torch.cat(text)
+    return text, offsets, label
 
 
 class DataModule(LightningDataModule):
     data_dir: str
     batch_size: int
 
-    train_dataset: Dataset
-    val_dataset: Dataset
-    test_dataset: Dataset
+    train_dataset: List[Tuple[List[float], torch.Tensor]]
+    val_dataset: List[Tuple[List[float], torch.Tensor]]
+    test_dataset: List[Tuple[List[float], torch.Tensor]]
 
     def __init__(self, data_dir: str, batch_size: int):
         super().__init__()
@@ -54,21 +65,43 @@ class DataModule(LightningDataModule):
         train_len = int(train_val_len * 0.95)
         val_len = train_val_len - train_len
 
-        train_dataset, val_dataset = random_split(train_val_dataset, [train_len, val_len])
+        train_dataset, val_dataset, = random_split(train_val_dataset, [train_len, val_len])
+
+        #
+        # Transform datasets
+        #
+
+        context_field.build_vocab(train_val_dataset)
+        vocab = context_field.vocab
+
+        transformed_train_dataset = [(
+            [float(x.is_male), float(x.is_married), float(x.is_american), float(x.is_actor)],
+            torch.tensor([vocab[t] for t in x.context])
+        ) for x in train_dataset]
+
+        transformed_val_dataset = [(
+            [float(x.is_male), float(x.is_married), float(x.is_american), float(x.is_actor)],
+            torch.tensor([vocab[t] for t in x.context])
+        ) for x in val_dataset]
+
+        transformed_test_dataset = [(
+            [float(x.is_male), float(x.is_married), float(x.is_american), float(x.is_actor)],
+            torch.tensor([vocab[t] for t in x.context])
+        ) for x in test_dataset]
 
         #
         # Store datasets
         #
 
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.test_dataset = test_dataset
+        self.train_dataset = transformed_train_dataset
+        self.val_dataset = transformed_val_dataset
+        self.test_dataset = transformed_test_dataset
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, collate_fn=generate_batch)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, collate_fn=generate_batch)
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, collate_fn=generate_batch)
