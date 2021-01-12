981defab33bef5f9183b673d23b66cc7d543374f
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-12 14:47:05 +0100

    Add precision, recall and f1 metrics to training

--- src/ower/classifier.py
+++ src/ower/classifier.py
@@ -1,3 +1,4 @@
+import pytorch_lightning as pl
 import torch
 from pytorch_lightning import LightningModule
 from torch import nn
@@ -21,6 +22,11 @@ class Classifier(LightningModule):
         self.fc.weight.data.uniform_(-initrange, initrange)
         self.fc.bias.data.zero_()
 
+        # Add metrics
+        self.prec = pl.metrics.Precision(num_classes=100, multilabel=True)
+        self.recall = pl.metrics.Recall(num_classes=100, multilabel=True)
+        self.f1 = pl.metrics.F1(num_classes=100, multilabel=True)
+
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), lr=4.0)
 
@@ -37,4 +43,15 @@ class Classifier(LightningModule):
         output_batch = self(text_batch, offsets_batch)
         loss = criterion(output_batch, labels_batch)
 
+        # Update metrics
+        self.prec(output_batch, labels_batch)
+        self.recall(output_batch, labels_batch)
+        self.f1(output_batch, labels_batch)
+
         return loss
+
+    def training_epoch_end(self, outs):
+        print()
+        print('Precision', self.prec.compute())
+        print('Recall', self.recall.compute())
+        print('F1', self.f1.compute())
