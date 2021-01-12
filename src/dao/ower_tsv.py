31d6dfa168f452a43208907efb056025ba9c1b70
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-12 10:49:08 +0100

    Support variable number of classes

--- src/dao/ower_tsv.py
+++ src/dao/ower_tsv.py
@@ -3,6 +3,12 @@ from typing import List
 
 def write_ower_tsv(ower_tsv: str, rows: List) -> None:
     with open(ower_tsv, 'w', encoding='utf-8') as f:
-        for ent, label_male, label_married, label_american, label_actor, context in rows:
-            f.write('{}\t{}\t{}\t{}\t{}\t{}\n'
-                    .format(ent, label_male, label_married, label_american, label_actor, context.replace('"', '')))
+        for row in rows:
+            ent = row[0]
+            classes = row[1:-1]
+            context = row[-1]
+
+            f.write(str(ent))
+            for class_ in classes:
+                f.write(f'\t{str(class_)}')
+            f.write(f'\t{context}\n')
