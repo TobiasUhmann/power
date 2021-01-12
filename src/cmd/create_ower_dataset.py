758c1869ffba680a891db27eba6b82c2e7678ac3
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-12 07:27:41 +0100

    Take over and generalize save triples part from create_dataset.py

--- src/cmd/create_ower_dataset.py
+++ src/cmd/create_ower_dataset.py
@@ -1,7 +1,10 @@
 from argparse import ArgumentParser
+from os import makedirs, path
 from os.path import isdir
+from sqlite3 import connect
 from typing import List, Tuple
 
+from dao.triples_db import create_triples_table, insert_triple, DbTriple
 from dao.triples_txt import load_triples
 
 
@@ -15,9 +18,14 @@ def main() -> None:
     parser.add_argument('ryn_dataset_dir', metavar='ryn-dataset-dir',
                         help='Path to (input) Ryn Dataset Directory')
 
+    default_work_dir = 'work/'
+    parser.add_argument('--work-dir', metavar='STR', default=default_work_dir,
+                        help='Working Directory (default: {})'.format(default_work_dir))
+
     args = parser.parse_args()
 
     ryn_dataset_dir = args.ryn_dataset_dir
+    work_dir = args.work_dir
 
     #
     # Print applied config
@@ -25,6 +33,8 @@ def main() -> None:
 
     print('Applied config:')
     print('    {:20} {}'.format('ryn-dataset-dir', ryn_dataset_dir))
+    print()
+    print('    {:20} {}'.format('--work-dir', work_dir))
     print()
 
     #
@@ -36,19 +46,34 @@ def main() -> None:
         exit()
 
     #
+    # Create Working Directory if it does not exist already
+    #
+
+    makedirs(work_dir, exist_ok=True)
+
+    train_triples_db = path.join(work_dir, 'train_triples.db')
+    valid_triples_db = path.join(work_dir, 'valid_triples.db')
+    test_triples_db = path.join(work_dir, 'test_triples.db')
+
+    #
     # Run actual program
     #
 
-    create_ower_dataset(ryn_dataset_dir)
+    create_ower_dataset(ryn_dataset_dir, train_triples_db, valid_triples_db, test_triples_db)
 
 
-def create_ower_dataset(ryn_dataset_dir: str) -> None:
+def create_ower_dataset(
+        ryn_dataset_dir: str,
+        train_triples_db: str,
+        valid_triples_db: str,
+        test_triples_db: str
+) -> None:
     #
     # Load triples from Triples TXTs
     #
 
     print()
-    print('Load triples...')
+    print('Load triples from Triples TXTs...')
 
     train_triples_file = f'{ryn_dataset_dir}/split/cw.train2id.txt'
     valid_triples_file = f'{ryn_dataset_dir}/split/ow.valid2id.txt'
@@ -57,6 +82,30 @@ def create_ower_dataset(ryn_dataset_dir: str) -> None:
     train_triples: List[Tuple[int, int, int]] = load_triples(train_triples_file)
     valid_triples: List[Tuple[int, int, int]] = load_triples(valid_triples_file)
     test_triples: List[Tuple[int, int, int]] = load_triples(test_triples_file)
+
+    print('Done')
+
+    #
+    # Save triples to Triples DBs
+    #
+
+    print()
+    print('Save triples to Triples DBs...')
+
+    with connect(train_triples_db) as conn:
+        create_triples_table(conn)
+        for triple in train_triples:
+            insert_triple(conn, DbTriple(triple[0], triple[1], triple[2]))
+
+    with connect(valid_triples_db) as conn:
+        create_triples_table(conn)
+        for triple in valid_triples:
+            insert_triple(conn, DbTriple(triple[0], triple[1], triple[2]))
+
+    with connect(test_triples_db) as conn:
+        create_triples_table(conn)
+        for triple in test_triples:
+            insert_triple(conn, DbTriple(triple[0], triple[1], triple[2]))
 
     print('Done')
 
