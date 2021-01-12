0260414d960d4941cfa96beca11185b8c2a0da49
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-12 11:20:02 +0100

    Rename power -> ower && Mark data/ as data directory

--- src/cmd/train_classifier.py
+++ src/cmd/train_classifier.py
@@ -2,8 +2,8 @@ from argparse import ArgumentParser
 
 from pytorch_lightning import Trainer
 
-from power.classifier import Classifier
-from power.data_module import DataModule
+from ower.classifier import Classifier
+from ower.data_module import DataModule
 
 
 def main():
