43d846568b51b2b05b7308eb3b884f1c481a806e
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-12 08:14:51 +0100

    Fix paths and get entities for each class

--- src/cmd/create_ower_dataset.py
+++ src/cmd/create_ower_dataset.py
@@ -1,11 +1,13 @@
 from argparse import ArgumentParser
+from collections import defaultdict
 from os import makedirs, path
-from os.path import isdir
+from os.path import isdir, isfile
 from sqlite3 import connect
 from typing import List, Tuple, Dict, Set
 
+from dao.classes_tsv import load_classes
 from dao.contexts_txt import load_contexts
-from dao.triples_db import create_triples_table, insert_triple, DbTriple
+from dao.triples_db import create_triples_table, insert_triple, DbTriple, select_entities_with_class
 from dao.triples_txt import load_triples
 
 
@@ -19,6 +21,9 @@ def main() -> None:
     parser.add_argument('ryn_dataset_dir', metavar='ryn-dataset-dir',
                         help='Path to (input) Ryn Dataset Directory')
 
+    parser.add_argument('classes_tsv', metavar='classes-tsv',
+                        help='Path to (input) Classes TSV')
+
     default_work_dir = 'work/'
     parser.add_argument('--work-dir', metavar='STR', default=default_work_dir,
                         help='Path to (output) Working Directory (default: {})'.format(default_work_dir))
@@ -26,6 +31,7 @@ def main() -> None:
     args = parser.parse_args()
 
     ryn_dataset_dir = args.ryn_dataset_dir
+    classes_tsv = args.classes_tsv
     work_dir = args.work_dir
 
     #
@@ -34,6 +40,7 @@ def main() -> None:
 
     print('Applied config:')
     print('    {:20} {}'.format('ryn-dataset-dir', ryn_dataset_dir))
+    print('    {:20} {}'.format('classes-tsv', classes_tsv))
     print()
     print('    {:20} {}'.format('--work-dir', work_dir))
     print()
@@ -46,13 +53,21 @@ def main() -> None:
         print('Ryn Dataset Directory not found')
         exit()
 
-    train_triples_txt = path.join(ryn_dataset_dir, 'split', 'ow.train2id.txt')
+    train_triples_txt = path.join(ryn_dataset_dir, 'split', 'cw.train2id.txt')
     valid_triples_txt = path.join(ryn_dataset_dir, 'split', 'ow.valid2id.txt')
     test_triples_txt = path.join(ryn_dataset_dir, 'split', 'ow.test2id.txt')
 
     train_contexts_txt = path.join(ryn_dataset_dir, 'text', 'cw.train-sentences.txt')
-    valid_contexts_txt = path.join(ryn_dataset_dir, 'text', 'cw.valid-sentences.txt')
-    test_contexts_txt = path.join(ryn_dataset_dir, 'text', 'cw.test-sentences.txt')
+    valid_contexts_txt = path.join(ryn_dataset_dir, 'text', 'ow.valid-sentences.txt')
+    test_contexts_txt = path.join(ryn_dataset_dir, 'text', 'ow.test-sentences.txt')
+
+    #
+    # Assert that (input) Classes TSV exists
+    #
+
+    if not isfile(classes_tsv):
+        print('Classes TSV not found')
+        exit()
 
     #
     # Create (output) Working Directory if it does not exist already
@@ -69,7 +84,7 @@ def main() -> None:
     #
 
     create_ower_dataset(train_triples_txt, valid_triples_txt, test_triples_txt, train_contexts_txt, valid_contexts_txt,
-                        test_contexts_txt, train_triples_db, valid_triples_db, test_triples_db)
+                        test_contexts_txt, classes_tsv, train_triples_db, valid_triples_db, test_triples_db)
 
 
 def create_ower_dataset(
@@ -79,6 +94,7 @@ def create_ower_dataset(
         train_contexts_txt: str,
         valid_contexts_txt: str,
         test_contexts_txt: str,
+        classes_tsv: str,
         train_triples_db: str,
         valid_triples_db: str,
         test_triples_db: str
@@ -132,6 +148,31 @@ def create_ower_dataset(
     test_contexts: Dict[int, Set[str]] = load_contexts(test_contexts_txt)
 
     print('Done')
+
+    #
+    # Get classes for each entity
+    #
+
+    print()
+    print('Load contexts from Contexts TXTs...')
+
+    classes: List[Tuple[int, int]] = load_classes(classes_tsv)
+
+    train_class_to_entities = defaultdict(set)
+    valid_class_to_entities = defaultdict(set)
+    test_class_to_entities = defaultdict(set)
+
+    with connect(train_triples_db) as conn:
+        for class_ in classes:
+            train_class_to_entities[class_] = select_entities_with_class(conn, class_)
+
+    with connect(valid_triples_db) as conn:
+        for class_ in classes:
+            valid_class_to_entities[class_] = select_entities_with_class(conn, class_)
+
+    with connect(test_triples_db) as conn:
+        for class_ in classes:
+            test_class_to_entities[class_] = select_entities_with_class(conn, class_)
 
 
 if __name__ == '__main__':
