ed545116779a2bb98ebddcee53b4a50f9a2f3d1a
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-02-02 12:43:48 +0100

    Complete OWER DAO refactoring

--- src/dao/ower/ower_dir.py
+++ src/dao/ower/ower_dir.py
@@ -43,9 +43,11 @@ v1
         samples-v1-valid.tsv
 
 """
+
 from os.path import isdir
 from pathlib import Path
 
+from dao.ower.samples_tsv import SamplesTsv
 from dao.ower.triples_db import TriplesDb
 
 
@@ -56,6 +58,10 @@ class OwerDir:
     tain_triples_db: TriplesDb
     valid_triples_db: TriplesDb
     test_triples_db: TriplesDb
+
+    train_samples_tsv: SamplesTsv
+    valid_samples_tsv: SamplesTsv
+    test_samples_tsv: SamplesTsv
     
     def __init__(self, name: str, path: Path):
         self.name = name
@@ -64,6 +70,10 @@ class OwerDir:
         self.train_triples_db = TriplesDb('Train Triples DB', path.joinpath('triples-v1-train.db'))
         self.valid_triples_db = TriplesDb('Valid Triples DB', path.joinpath('triples-v1-valid.db'))
         self.test_triples_db = TriplesDb('Test Triples DB', path.joinpath('triples-v1-test.db'))
+        
+        self.train_samples_tsv = SamplesTsv('Train Samples TSV', path.joinpath('samples-v2-train.tsv'))
+        self.valid_samples_tsv = SamplesTsv('Valid Samples TSV', path.joinpath('samples-v2-valid.tsv'))
+        self.test_samples_tsv = SamplesTsv('Test Samples TSV', path.joinpath('samples-v2-test.tsv'))
 
     def check(self) -> None:
         if not isdir(self.path):
@@ -73,4 +83,7 @@ class OwerDir:
         self.train_triples_db.check()
         self.valid_triples_db.check()
         self.test_triples_db.check()
-        
+
+        self.train_samples_tsv.check()
+        self.valid_samples_tsv.check()
+        self.test_samples_tsv.check()
--- src/dao/ower/samples_tsv.py
+++ src/dao/ower/samples_tsv.py
@@ -2,19 +2,34 @@
 Provides functions to load/save a Samples TSV.
 """
 
+from os.path import isfile
+from pathlib import Path
 from typing import List
 
 
-def write_samples_tsv(samples_tsv: str, rows: List) -> None:
-    with open(samples_tsv, 'w', encoding='utf-8') as f:
-        for row in rows:
-            ent = row[0]
-            classes = row[1:-1]
-            sentences = row[-1]
+class SamplesTsv:
+    name: str
+    path: Path
 
-            f.write(str(ent))
-            for class_ in classes:
-                f.write(f'\t{str(class_)}')
-            for sentence in sentences:
-                f.write(f'\t{sentence}')
-            f.write('\n')
+    def __init__(self, name: str, path: Path):
+        self.name = name
+        self.path = path
+
+    def check(self) -> None:
+        if not isfile(self.path):
+            print(f'{self.name} not found')
+            exit()
+
+    def write_samples_tsv(self, rows: List) -> None:
+        with open(self.path, 'w', encoding='utf-8') as f:
+            for row in rows:
+                ent = row[0]
+                classes = row[1:-1]
+                sentences = row[-1]
+
+                f.write(str(ent))
+                for class_ in classes:
+                    f.write(f'\t{str(class_)}')
+                for sentence in sentences:
+                    f.write(f'\t{sentence}')
+                f.write('\n')
