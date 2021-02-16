89803dc083aceb00e0ae0114d382c71d24d88569
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-02-16 13:14:45 +0100

    Write image to tensorboard

--- src/snippets/tensorboard_tutorial/tensorboard_tutorial.py
+++ src/snippets/tensorboard_tutorial/tensorboard_tutorial.py
@@ -73,6 +73,32 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    from torch.utils.tensorboard import SummaryWriter
+
+    #
+    # Tensorboard Setup
+    #
+
+    # default `log_dir` is "runs" - we'll be more specific here
+    writer = SummaryWriter('runs/fashion_mnist_experiment_1')
+
+    #
+    # Write to Tensorboard
+    #
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # create grid of images
+    img_grid = torchvision.utils.make_grid(images)
+
+    # show images
+    matplotlib_imshow(img_grid, one_channel=True)
+
+    # write to tensorboard
+    writer.add_image('four_fashion_mnist_images', img_grid)
+
 
 if __name__ == '__main__':
     main()
