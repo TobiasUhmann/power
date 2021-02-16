187c8a5de37e3af61f5278165dc41d4ae65c052f
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-02-16 12:58:55 +0100

    Define net, criterion and optimizer

--- src/snippets/tensorboard_tutorial.py
+++ src/snippets/tensorboard_tutorial.py
@@ -49,6 +49,30 @@ def main():
         else:
             plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 4 * 4, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 16 * 4 * 4)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
 
 if __name__ == '__main__':
     main()
