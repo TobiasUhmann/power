8293ca04a8dd7c6ca481a7ffa53b7dc924170f1d
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-02-20 12:30:34 +0100

    Log train/valid loss & Optimize logging performance

--- src/ower/util.py
+++ src/ower/util.py
@@ -4,13 +4,15 @@ from torch import Tensor
 
 
 def log_tensor(module_path: str, tensor_str: str, tensor: Tensor, shape_str: str) -> None:
-    logging.debug(
-        f'{module_path}' '\n'
-        f'{tensor_str} {shape_str}' '\n'
-        f'{tensor}')
+    if logging.root.level <= logging.DEBUG:
 
-    if tensor.requires_grad:
-        tensor.register_hook(lambda grad: logging.debug(
+        logging.debug(
             f'{module_path}' '\n'
-            f'{tensor_str}.grad {shape_str}' '\n'
-            f'{grad}'))
+            f'{tensor_str} {shape_str}' '\n'
+            f'{tensor}')
+
+        if tensor.requires_grad:
+            tensor.register_hook(lambda grad: logging.debug(
+                f'{module_path}' '\n'
+                f'{tensor_str}.grad {shape_str}' '\n'
+                f'{grad}'))
