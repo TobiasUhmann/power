ec2aed47a0e853c733893e8513380ff3b2b1e062
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-03-06 16:22:49 +0100

    Bump version to 0.26.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Tobias Uhmann'
 author = 'Tobias Uhmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.25.0'
+release = '0.26.0'
 
 # -- General configuration ---------------------------------------------------
 
