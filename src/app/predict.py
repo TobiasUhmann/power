5a8d6d8e48245524004727232ec76701e715bfcd
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-05-27 23:44:35 +0200

    Implement Predict page

--- src/app/predict.py
+++ src/app/predict.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
-from pprint import pformat
-from typing import List
+from typing import List, Set, Tuple
 
 import streamlit as st
 
@@ -8,9 +6,10 @@ from app.common import add_sidebar_param_split_dir, add_sidebar_param_text_dir, 
     add_sidebar_param_texter_pkl
 from data.irt.text.text_dir import TextDir
 from data.power.ruler_pkl import RulerPkl
+from data.power.split.facts_tsv import Fact
 from data.power.split.split_dir import SplitDir
 from data.power.texter_pkl import TexterPkl
-from models.fact import Fact
+from models.ent import Ent
 from power.aggregator import Aggregator
 
 
@@ -19,7 +18,7 @@ def add_predict_page():
     split_dir, text_dir, ruler_pkl, texter_pkl = _add_sidebar()
 
     st.title('Predict')
-    _add_main_page(split_dir, text_dir, ruler_pkl, texter_pkl)
+    _create_main_page(split_dir, text_dir, ruler_pkl, texter_pkl)
 
 
 def _add_sidebar():
@@ -33,47 +32,130 @@ def _add_sidebar():
     return split_dir, text_dir, ruler_pkl, texter_pkl
 
 
-def _add_main_page(split_dir: SplitDir, text_dir: TextDir, ruler_pkl: RulerPkl, texter_pkl: TexterPkl) -> None:
-    ent_to_lbl = split_dir.test_entities_tsv.load()
+def _create_main_page(split_dir: SplitDir, text_dir: TextDir, ruler_pkl: RulerPkl, texter_pkl: TexterPkl) -> None:
+    st.write("Select a valid or test entity, view its known and unknown facts"
+             " as well as its texts, and observe the Power model's predictions.")
 
-    st.write(ent_to_lbl)
+    st.write("Ideally, the top predictions should match the entity's unknown"
+             " facts. The predictions can be expanded to see the model's eplanation"
+             " for the predicted fact, i.e. the rules and/or text prioritization,"
+             " depending on whether the fact has been predicted by Ruler and/or"
+             " Texter.")
 
-    ent = st.number_input('Entity ID', min_value=0)
+    ent, subset = _select_entity(split_dir)
 
-    #
-    # Test Facts
-    #
+    _show_entity_facts(split_dir, ent, subset)
 
-    st.title('Test Facts')
+    ent_texts = _show_entity_texts(text_dir, ent, subset)
 
-    test_facts_known: List[Fact] = split_dir.test_facts_known_tsv.load()
+    _show_predictions(ruler_pkl, texter_pkl, ent, ent_texts)
 
-    ent_facts_known = [fact for fact in test_facts_known if fact.head == ent]
 
-    for fact in ent_facts_known:
-        st.write(fact)
+def _select_entity(split_dir: SplitDir) -> Tuple[Ent, str]:
+    """
+    Show
+    - Radio buttons that allows switching between valid and test entities
+    - Spinner to select entity by ID
+    - Text field that shows the selected entity's label
+    """
 
-    #
-    # Texts
-    #
+    st.header('Entity')
 
-    st.title('Texts')
+    valid_test_selection = st.radio('', ['Valid Entity', 'Test Entity'])
 
-    ent_to_texts = text_dir.ow_test_sents_txt.load()
-    ent_texts = ent_to_texts[ent]
+    if valid_test_selection == 'Valid Entity':
+        subset = 'valid'
+        ent_to_lbl = split_dir.valid_entities_tsv.load()
 
-    for ent_text in ent_texts:
-        st.write(ent_text)
+    elif valid_test_selection == 'Test Entity':
+        subset = 'test'
+        ent_to_lbl = split_dir.test_entities_tsv.load()
 
-    #
-    # Predict
-    #
+    else:
+        raise ValueError()
+
+    min_id = min(ent_to_lbl.keys())
+    max_id = max(ent_to_lbl.keys())
+
+    cols = st.beta_columns([25, 75])
+    ent_id = cols[0].number_input('ID', key='_ent_id', min_value=min_id, max_value=max_id, value=min_id)
+    cols[1].text_input('Label', key='_ent_lbl', value=ent_to_lbl[ent_id])
+
+    ent = Ent(ent_id, ent_to_lbl[ent_id])
+
+    return ent, subset
+
+
+def _show_entity_facts(split_dir: SplitDir, ent: Ent, subset: str) -> None:
+    if subset == 'valid':
+        known_facts: List[Fact] = split_dir.valid_facts_known_tsv.load()
+        unknown_facts: List[Fact] = split_dir.valid_facts_unknown_tsv.load()
+    elif subset == 'test':
+        known_facts: List[Fact] = split_dir.test_facts_known_tsv.load()
+        unknown_facts: List[Fact] = split_dir.test_facts_unknown_tsv.load()
+    else:
+        raise ValueError()
+
+    known_ent_facts = [fact for fact in known_facts if fact.head == ent.id]
+    unknown_ent_facts = [fact for fact in unknown_facts if fact.head == ent.id]
+
+    st.header('Known Facts')
+    for fact in known_ent_facts:
+        st.write(f'{fact.head_lbl}, {fact.rel_lbl}, {fact.tail_lbl}')
+
+    st.header('Unknown Facts')
+    for fact in unknown_ent_facts:
+        st.write(f'{fact.head_lbl}, {fact.rel_lbl}, {fact.tail_lbl}')
+
+
+def _show_entity_texts(text_dir: TextDir, ent: Ent, subset: str) -> Set[str]:
+    if subset == 'valid':
+        ent_to_texts = text_dir.ow_valid_sents_txt.load()
+    elif subset == 'test':
+        ent_to_texts = text_dir.ow_test_sents_txt.load()
+    else:
+        raise ValueError()
+
+    ent_texts = ent_to_texts[ent.id]
+
+    st.header('Texts')
+    for i, text in enumerate(ent_texts):
+        st.text_area(label='', value=text, key=f'{ent.id}_{i}')
+
+    return ent_texts
+
+
+def _show_predictions(ruler_pkl: RulerPkl, texter_pkl: TexterPkl, ent: Ent, ent_texts: Set[str]):
+    """
+    Load the Power model, make predictions for the selected entity,
+    and list the predictions as expandable sections that reveal the
+    predictions' rules and text prioritizations.
+    """
+
+    st.header('Predictions')
 
     ruler = ruler_pkl.load()
     texter = texter_pkl.load().cpu()
 
-    power = Aggregator(texter, ruler, alpha=1.0)
+    power = Aggregator(texter, ruler, alpha=0.5)
 
     preds = power.predict(ent, list(ent_texts))
+
     for pred in preds:
-        st.write(pred.fact.head, pred.fact.rel, pred.fact.tail, pred.conf)
+        expander_title = '{:.2f}% - {}, {}, {}'.format(
+            pred.conf * 100, pred.fact.head.lbl, pred.fact.rel.lbl, pred.fact.tail.lbl)
+
+        with st.beta_expander(expander_title, expanded=False):
+            st.subheader('Rules')
+            if pred.rules:
+                for rule in pred.rules:
+                    st.write(rule)
+            else:
+                st.write('None')
+
+            st.subheader('Texts')
+            if pred.sents:
+                for sent in pred.sents:
+                    st.write(sent)
+            else:
+                st.write('None')
